150fe41a847aa3baf647719cd5043dfcc8b1d530
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2024-02-17 15:40:01 -0500

    Removed routes in auth.py
    Removed routes for HTML from Calculatron project.

--- Taskhub/Flask Web App/website/auth.py
+++ Taskhub/Flask Web App/website/auth.py
@@ -1,5 +1,4 @@
-import random
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash
 import sqlite3
 
 auth = Blueprint('auth', __name__)
@@ -64,76 +63,3 @@ def sign_up():
 @auth.route('/about')
 def about():
     return render_template("about.html")
-
-# Applications
-@auth.route('/applications')
-def applications():
-    return render_template("applications.html")
-
-# Calculator Application
-@auth.route('/calculatorApp', methods=['GET', 'POST'])
-def calculator():
-    if request.method == 'POST':
-        # Handle calculation here
-        try:
-            expr = request.form['expression']
-            result = eval(expr)
-        except:
-            result = 'E E E'
-        return render_template('calculatorApp.html', result=result)
-    return render_template('calculatorApp.html')
-
-# Random Number Application [GET]
-@auth.route('/randMathApp', methods=['GET'])
-def get_randMathApp():
-    # Declaring Variables
-    num1 = random.randint(1, 10)
-    num2 = random.randint(1, 10)
-    operator = random.choice(['+', '-', '*', '/'])
-    question = f"{num1} {operator} {num2}"
-    try: # Create Calculatons based on operator
-        if operator == '+':
-            correct_answer = num1 + num2
-        elif operator == '-':
-            correct_answer = num1 - num2
-        elif operator == '*':
-            correct_answer = num1 * num2
-        elif operator == '/':
-            correct_answer = num1 / num2
-            correct_answer = round(correct_answer, 2)
-    except ZeroDivisionError:
-        return "Error: Division by zero is not allowed."
-    return render_template("randMathApp.html", question=question, correct_answer = correct_answer)
-
-# Random Number Application [POST]
-@auth.route('/randMathApp', methods=['POST'])
-def post_randMathApp():
-    # Declaring variables
-    user_answer = request.form.get("user_answer")
-    correct_answer = request.form.get("correct_answer")
-    result = " "
-    try:
-        if user_answer == correct_answer:
-            result = "Correct!"
-        else:
-            result = "Incorrect. Try again."
-    except ValueError: # user_answer did not contain an integer
-        result = "Invalid number!"
-    return render_template("randMathResult.html", correct_answer=correct_answer, result=result, user_answer=user_answer)
-
-# Memory Bank
-@auth.route('/memBankApp', methods=['GET', 'POST'])
-def mem_bank_app():
-    if request.method == 'POST':
-        if 'equation' in request.form:
-            equation = request.form['equation']
-            equations.append(equation)
-        elif 'clear' in request.form:
-            # Handle clearing the list
-            equations.clear()
-        return redirect(url_for('auth.mem_bank_app'))  # Redirect to the same route after form submission
-    return render_template('memBankApp.html')
-
-@auth.route('/memBankDisplay')
-def memBankDisplay():
-    return render_template("memBankDisplay.html", equations=equations)
